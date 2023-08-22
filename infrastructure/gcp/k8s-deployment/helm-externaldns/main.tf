ff67be75e23def5c7ef253c565c085dc310dcd36
greyhats13 <imam.arief.rhmn@gmail.com>
2023-08-22 18:32:13 +0700

    fix folder naming

---
+++ infrastructure/gcp/k8s-deployment/helm-externaldns/main.tf
@@ -0,0 +1,65 @@
+# Terraform State Storage
+terraform {
+  backend "gcs" {
+    bucket = "ols-dev-gcloud-storage-tfstate"
+    prefix = "helm/ols-dev-helm-external-dns"
+  }
+}
+
+data "google_project" "current" {}
+
+# create a GKE cluster with 2 node pools
+data "terraform_remote_state" "gcloud_dns_ols" {
+  backend = "gcs"
+
+  config = {
+    bucket = "ols-dev-gcloud-storage-tfstate"
+    prefix = "gcloud-dns/ols-dev-gcloud-dns-blast"
+  }
+}
+
+module "externaldns" {
+  source                 = "../../modules/compute/helm"
+  region                 = "asia-southeast2"
+  unit                   = "ols"
+  env                    = "dev"
+  code                   = "helm"
+  feature                = "external-dns"
+  release_name           = "external-dns"
+  repository             = "https://charts.bitnami.com/bitnami"
+  chart                  = "external-dns"
+  create_service_account = true
+  project_id             = data.google_project.current.project_id
+  service_account_role   = "roles/dns.admin"
+  kubernetes_cluster_role_rules = {
+    api_groups = [""]
+    resources  = ["services", "endpoints", "pods"]
+    verbs      = ["get", "list", "watch"]
+  }
+  values = []
+  helm_sets = [
+    # Dont create service acocunt
+    {
+      name = "serviceAccount.create"
+      value = false
+    },
+    # Use existing service account if create_service_account is set to true
+    {
+      name = "serviceAccount.name"
+      value = "ols-dev-helm-external-dns"
+    },
+    {
+      name  = "provider"
+      value = "google"
+    },
+    {
+      name  = "google.project"
+      value = data.google_project.current.project_id
+    },
+    {
+      name  = "zoneVisibility"
+      value = data.terraform_remote_state.gcloud_dns_ols.outputs.dns_zone_visibility
+    }
+  ]
+  namespace        = "ingress"
+}
